ef5c79154b39cc324edd4c9dac22ee72703136f4
Dinesh <dineshigdd@gmail.com>
2024-05-09 19:58:28 -0700

    remove test comment

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,7 +15,7 @@ export default function Hero( props: HeroProps){
                 src={ props.imgData }
                 alt={ props.imgAlt }
                 fill
-                style={{objectFit:'cover'}}/*test */
+                style={{objectFit:'cover'}}
                 />
             </div>
             <div className="absolute inset-0 bg-gradient-to-r from-slate-900"></div>

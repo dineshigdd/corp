dd7ea98df117fd35fdb4c39e8bd6ebc46790ad69
Dinesh <dineshigdd@gmail.com>
2024-05-09 18:52:55 -0700

    add initial files from linux

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import homeImg from "public/home.jpg";
+import Hero from '@/components/hero';
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero
+        imgData= { homeImg }
+        imgAlt="Car factory"
+        title="Professional Cloud Hosting"
+      />      
+    </div>
+    
+  );
+}
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,7 @@
+
+export default function ScalePage() {
+    return (
+      <div>ScalePage Page</div>
+    )
+  }
+  
